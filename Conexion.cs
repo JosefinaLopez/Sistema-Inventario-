6610afb7e32793c715aff2958e89d8419e0dd240
Josefina Lopez <Josefinalopez5900@gmail.com>
2023-02-02 12:10:54 -0600

    first commit

---
+++ Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        private SqlConnection conexion = new SqlConnection("server = JOSEFINALOPEZ; database = Inventario; integrated security = true");
+
+        public SqlConnection OpenConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+
+            return conexion;
+        }
+        public SqlConnection CloseConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+        
+    }
+}
